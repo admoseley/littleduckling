b7ad8b9f688321698aa3bcd5babf40fc4f82a586
Adrian Moseley <adrian.moseley@gmail.com>
2018-12-05 21:34:29 -0600

    Add Date Function

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 8000;
+var dt = require('./date');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('Hello World\n');
+  res.write("The date and time are currently: " + dt.myDateTime() + "<br />");
+  res.end('\nHello World\n');
 });
 
 server.listen(port, hostname, () => {
